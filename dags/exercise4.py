481346a1beffc5445f86e8f8764d8a9ff34b0015
Bjorn Rijpert <bjorn.rijpert@cg.nl>
2019-11-22 15:03:49 +0100

    Update exercise4.py

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -1,1 +1,29 @@
 
+#september 20th
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator 
+from airflow.operators.dummy_operator import DummyOperator 
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import datetime
+
+from decimal import Decimal
+import time
+
+import sys
+
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.utils.decorators import apply_defaults
+from airflow.contrib.operators.sql_to_gcs import BaseSQLToGoogleCloudStorageOperator
+
+args = {"owner": "Bjorn_Rijpert", "start_date": "2019-09-20"}
+
+dag = DAG( 
+  dag_id="exercise3", 
+   default_args=args, 
+   start_date=datetime.today(),
+   schedule_interval="@daily",
+)
+
